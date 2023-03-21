84a691c32a99f45e67c01351af6f5fc7bdc3e72c
Lera210621 <lera.vakulenko.012106@gmail.com>
2023-03-21 19:39:50 +0300

    D3_news

---
+++ project/news/templatetags/custom_filters.py
@@ -0,0 +1,34 @@
+from django import template
+
+
+register = template.Library()
+
+
+CURRENCIES_SYMBOLS = {
+   'rub': 'Р',
+   'usd': '$',
+}
+
+
+@register.filter()
+def currency(value, code='rub'):
+   """
+   value: значение, к которому нужно применить фильтр
+   code: код валюты
+   """
+   postfix = CURRENCIES_SYMBOLS[code]
+
+   return f'{value} {postfix}'
+
+
+@register.filter()
+def censor(value):
+    bad_words = ['idiot', 'stupid', 'donkey', 'Stupid']
+
+    if not isinstance(value, str):
+        raise TypeError(f"unresolved type '{type(value)}' expected  type 'str'")
+
+    for word in value.split():
+        if word.lower() in bad_words:
+            value = value.replace(word, f"{word[0]}{'*' * (len(word) - 1)}")
+    return value
